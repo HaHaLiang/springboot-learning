5c70e53b7be72340749778ce2731a73886d2c558
lianghaha <lianghaha525@sina.com>
2021-11-07 17:06:42 +0800

    分层

--- api/src/main/java/com/example/api/controller/HelloWorldController.java
+++ api/src/main/java/com/example/api/controller/HelloWorldController.java
@@ -34,4 +34,5 @@ public class HelloWorldController {
         return String.valueOf(SqrtUtil.getSqrtVal(value));
     }
 
+
 }
---
+++ repository/src/main/java/com/example/repository/RepositoryApplication.java
@@ -0,0 +1,11 @@
+package com.example.repository;
+
+
+
+public class RepositoryApplication {
+
+    public static void main(String[] args) {
+        //SpringApplication.run(RepositoryApplication.class, args);
+    }
+
+}
